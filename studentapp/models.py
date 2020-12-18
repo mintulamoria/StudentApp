1f37cb84c3d60d5a7d1dd1d1fa39f1ae1c92e156
mintulamoria <umesh@securitytroops.com>
2020-12-18 18:00:02 +0530

    status field add

--- studentapp/models.py
+++ studentapp/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+#from model_utils import FieldTracker
 
 
 
 class Student(models.Model):
+	STATUS_CHOICES = (
+		('draft', 'Draft'),
+		('published', 'Published'),
+	)
 	name = models.CharField(max_length=100)
 	father_name = models.CharField(max_length=100)
 	mother_name = models.CharField(max_length=100)
 	address = models.TextField(max_length=200)
 	created_date = models.DateTimeField(auto_now_add=True)
 	published_date = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10, 
+                              choices=STATUS_CHOICES,
+                              default='draft')
+
+	
+	class Meta: 
+		ordering = ('-publish',)
 
 	class Meta:
 		unique_together = ('name', 'father_name', 'mother_name',)
