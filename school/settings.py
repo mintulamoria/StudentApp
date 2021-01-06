524fec7ea9a65a44d1056de1e981d1db9e7e955f
mintulamoria <umesh@securitytroops.com>
2021-01-06 18:25:37 +0530

    Bootstrap use

--- school/settings.py
+++ school/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -114,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
