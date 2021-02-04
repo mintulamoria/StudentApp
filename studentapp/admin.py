9cfdd10f74c1d8a6e558f97840eb0101d04a60fa
mintulamoria <umesh@securitytroops.com>
2021-02-04 12:23:22 +0530

    Search and list display field add

--- studentapp/admin.py
+++ studentapp/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	list_display=('name', 'father_name', 'cover', 'status',)
+	search_fields=('name', 'father_name',)
+
+	def student_cover(self):
+		return '<cover src="%s"/>' % self.cover
+#	student_cover.allow_tags = True
+
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(StudentClassInfo)
